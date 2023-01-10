6684b18be28b0b477dff4690d3a6f189b9843655
Yesi78 <97620920+Yesi78@users.noreply.github.com>
2023-01-10 16:24:11 -0500

    Assignment_02
    2-17-21

--- Question01.java
+++ Question01.java
@@ -5,43 +5,97 @@
  */
 package question01;
 
-import java.util.Scanner;
-
 /**
- * Question 01
- * @author Yesenia
- * This program finds and prints pairs of positive integers (i, j) 
- * that fit the form i < j < n and(i^2+j^2+ 1)/(ij) is an integer
+ * This program takes a given array and sorts it. Then displays the sorted
+ * integer array.
+ * @author Yesenia Morsles
  */
 public class Question01 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = in.nextInt();
-        allpairs(n);
+    public static void main(String[] args) {    
+        System.out.println("Test Cases:");
+        System.out.println("");
+        testCases();
+        
     }
     
-/**
- * This method calculates ordered pairs that satisfy the equation
- * This method throws an Illegal Argument Exception when n is less than 1
- * @param n is the integer 
- */
-    public static void allpairs(int n) {
-        if (n < 2) {
-            throw new IllegalArgumentException("wrong number");
-        }
-        for (int j = 1; j < n; j++) {
-            for (int i = 1; i < j; i++) {
-                //checks whether the given equation is divided evenly
-                if (((Math.pow(j, 2) + Math.pow(i, 2) + 1) % (i * j)) == 0) {
-                    System.out.printf("\n (%d,%d)", i, j);
-                }
+    /**
+     * This method passes the given arrays as test cases to be sorted and displayed
+     */
+    public static void testCases() {
+        int[] arr1 = {1, 7, 4, 0};
+        int [] arr2 = {2, 4, 6, 3, 8, 10};
+        int[] arr3 = {5, 2, 6, 7, 8, 9};
+        int[] arr4 = {0,1,4,6,2,3,7,10};
+        int[] arr5 = {10,14,16,9,2,0,12};
+        int[] arr6 = {89,77,65,44,101,90,56};
+        int[] arr7 = {-1,0,-5,7,9,-10,11,4,6};
+        
+        System.out.print("Original: ");
+        displayArray(arr1);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr1));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr2);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr2));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr3);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr3));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr4);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr4));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr5);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr5));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr6);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr6));
+        System.out.println("");
+        System.out.print("Original: ");
+        displayArray(arr7);
+        System.out.print("Sorted: ");
+        displayArray(sort(arr7));
+        System.out.println("");
+    }
+
+    /**
+     * This method sorts the array through 
+     * @param a the integer array
+     * @return the sorted array
+     */
+    public static int[] sort(int[] a) {
+        for (int j = 1; j < a.length; j++) {
+            int key = a[j];
+            int i = j - 1;
+            while (i >= 0 && a[i] > key) {
+                a[i + 1] = a[i];
+                i--;
             }
+            a[i + 1] = key;
         }
+        return a;
+    }
+/**
+ * This method is used to display an array
+ * @param a is an integer array 
+ */
+    public static void displayArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
     }
 }
--- Question02.java
+++ Question02.java
@@ -7,55 +7,277 @@ package question02;
 
 import java.util.Scanner;
 
+import java.util.Random;
+
 /**
- *Question 02
- * @author Yesenia
- * This program approximates the are of a circle of a given radius using
- * the area of rectangles.
+ * This program asks for the size of a 2D integer array, m rows and n columns
+ * and populates it with random numbers from 0 to mn-1. Then it checks if is
+ * consecutive in any of the following patterns: horizontal, vertical, and two 
+ * diagonal directions. It also gives test cases to show each pattern and the
+ * programs effectiveness.
+ * @author Yesenia Morales
  */
 public class Question02 {
 
     /**
      * @param args the command line arguments
+     * This method throws an Illegal Argument Exception when at least one of 
+     * the two integers is less than 4
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("Enter two numbers greater than or equal to 4: ");
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int mn = m * n;
+        if (mn < 16)
+            throw new IllegalArgumentException("Wrong array size!!");
+        int j;
+        int[][] arr = new int[m][n];
+        for (int[] arr1 : arr) {
+            for (j = 0; j < arr1.length; j++) {
+                int ra = rand.nextInt(mn);
+                arr1[j] = ra;
+                System.out.printf(arr1[j] + "\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("Horizontally consecutive: " + horizontal(arr));
+        System.out.println("Vertically consecutive: " + vertical(arr));
+        System.out.println("Diagonally consecutive left to right: " + diagonalLeftToRight(arr));
+        System.out.println("Diagonally consecutive right to left: " + diagonalRightToLeft(arr));
+        System.out.println(" ");
+        System.out.println("Test Cases: ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(" ");
+        testCases();
+    }
+
+    /**
+     * This method creates test cases to check the following methods correctness
+     */
+    public static void testCases() {
+        int[][] arr1 = {{2, 5, 3, 5, 6, 3, 2}, {2, 7, 2, 6, 4, 0, 7}, {9, 3, 9, 8, 7, 6, 10}};
+        displayArray(arr1);
+        System.out.println("Horizontally consecutive: " + horizontal(arr1));
+        System.out.println(" ");
         
-        System.out.print("Enter a radius: ");
-        double radius = in.nextDouble();
+
+        int[][] arr2 = {{2, 4, 5, 9, 3}, {2, 2, 4, 6, 5}, {3, 7, 5, 2, 3}, {4, 6, 7, 2, 1}, {5, 3, 4, 2, 9}};
+        displayArray(arr2);
+        System.out.println("Vertically consecutive: " + vertical(arr2));
+        System.out.println(" ");
         
-        System.out.print("Enter number of rectangles (higher amount for better accuracy) :  ");
-        int numberOfRectangles = in.nextInt();
+
+        int[][] arr3 = {{1, 5, 6, 7, 8, 3}, {3, 2, 4, 4, 3, 1},
+            {7, 6, 3, 3, 3, 6}, {3, 1, 5, 6, 2, 2}, {4, 6, 8, 6, 3, 5}};
+        displayArray(arr3);
+        System.out.println("Diagonally consecutive left to right: " + diagonalLeftToRight(arr3));
+        System.out.println(" ");
         
-        System.out.println("Area of a circle with radius " + radius +
-                " is " + circleArea(radius, numberOfRectangles));
+        int[][] arr4 = {{6,6,7,6,4},{1,5,5,4,1},{6,4,4,2,7},{3,8,3,2,3}, {5,4,6,7,2}};
+        displayArray(arr4);
+        System.out.println("Diagonally consecutive right to left: " + diagonalRightToLeft(arr4));
+        System.out.println(" ");
+
     }
     
     /**
-     * This method calculates the width of each rectangle and uses that to 
-     * calculate the area of each rectangle to finally calculate the area.
-     * This method throws two Illegal Argument Exceptions when the radius is 
-     * negative and when the rectangles are less than 1.
-     * @param radius is the radius of the circle
-     * @param numOfRectangles is the number of rectangles 
-     * @return the area of the circle
+     * This method displays a given 2D array
+     * @param a is a 2D integer array
      */
-    public static double circleArea(double radius, int numOfRectangles) {
-        if (radius < 0) 
-            throw new IllegalArgumentException("negative radius!!");
-        
-        if (numOfRectangles < 1) 
-            throw new IllegalArgumentException("wrong number of rectangles!!");
-        
-        double widthOfRectangle = radius / numOfRectangles;
-        double heightOfRectangle;
-        double area = 0;
-        
-        for (double x = 0; x <= radius; x += widthOfRectangle) {
-           heightOfRectangle = Math.sqrt((radius * radius) - (x * x));
-           area += heightOfRectangle * widthOfRectangle;
+    public static void displayArray(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf(a[i][j] + "\t");
+            }
+            System.out.println("");
         }
-     return area * 4;
+    }
+    /**
+     * This method checks whether a 2D array is horizontally consecutive
+     * @param a a 2D integer array
+     * @return The Boolean value if the array is horizontally consecutive
+     */
+    public static boolean horizontal(int[][] a) {
+        //loop to check if consecutively increasing
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            count = 0;
+            for (int j = 0; j < a[i].length - 1; j++) {
+                if (a[i][j] == a[i][j + 1] - 1) {
+                    count++;
+                }
+            }
+        }
+        
+        //loop to check if consecutively decreasing
+        for (int i = 0; i < a.length; i++) {
+            count = 0;
+            for (int j = 0; j < a[i].length - 1; j++) {
+                if (a[i][j] == a[i][j + 1] + 1) {
+                    count++;
+                }
+            }
+
+        }
+        return count >= 3;
     }
     
+    /**
+     * This method checks whether the given 2D array is vertically consecutive
+     * @param a a 2D integer array
+     * @return The Boolean value if the array is vertically consecutive
+     */
+    public static boolean vertical(int[][] a) {
+        int count = 0;
+        
+        //loop to check if consecutively increasing
+        for (int col = 0; col < a[0].length; col++) {
+            count = 0;
+            for (int row = 0; row < a.length - 1; row++) {
+                if (a[row][col] == a[row + 1][col] - 1) {
+                    count++;
+                }
+
+            }
+            return count >= 3;
+        }
+        //loop to check if consecutively decreasing
+        for (int col = 0; col < a[0].length; col++) {
+            count = 0;
+            for (int row = 0; row < a.length - 1; row++) {
+                if (a[row][col] == a[row + 1][col] + 1) {
+                    count++;
+                }
+
+            }
+        }
+        return count >= 3;
+    }
+    
+    /**
+     * This method checks whether the given 2D array is diagonally consecutive
+     * from left to right.
+     * @param a is a 2D integer array
+     * @return The Boolean value if the array is diagonally consecutive
+     */
+    public static boolean diagonalLeftToRight(int[][] a) {
+        int k;
+        int rowCount = 0;
+        int count;
+        
+        //loop to check if consecutively increasing 
+        for (int row = 0; rowCount <= a.length * 2; row++) {    
+            rowCount++;
+            if (rowCount > a.length)
+                row = 0;
+            count = 0;
+            k = row;
+            for (int col = 0; col < a[0].length && k < a.length; col++) {
+                if (rowCount > a.length) {
+                    col = k + (rowCount - a.length);
+                }
+                while (k < a.length - 1 && col < a[0].length -1) {
+                    if (a[k][col] == a[k + 1][col + 1] + 1) {
+                        count++;
+                        if (count >= 3) {
+                            return true;
+                        }                    
+                    }
+                    k++;
+                    col++;
+                }
+            }
+        }
+           //loop to check if consecutively decreasing
+           for (int row = 0; rowCount <= a.length * 2; row++) {    
+            rowCount++;
+            if (rowCount > a.length)
+                row = 0;
+            count = 0;
+            k = row;
+            for (int col = 0; col < a[0].length && k < a.length; col++) {
+                if (rowCount > a.length) {
+                    col = k + (rowCount - a.length);
+                }
+                while (k < a.length - 1 && col < a[0].length -1) {
+                    if (a[k][col] == a[k + 1][col + 1] + 1) {
+                        count++;
+                        if (count >= 3) {
+                            return true;
+                        }                  
+                    }
+                    k++;
+                    col++;
+                }
+        }
+    }
+           return false;
+    }
+    
+    /**
+     * This method checks whether the given array is diagonally consecutive from 
+     * right to left 
+     * @param a is a 2D integer array
+     * @return the Boolean value if the array is diagonally consecutive
+     */
+    public static boolean diagonalRightToLeft(int[][] a) {
+        int k;
+        int rowCount = 0;
+        int count;
+        //loop to check if consecutively increasing
+        for (int row = 0; rowCount <= a.length * 2; row++) {    
+            rowCount++;
+            if (rowCount > a.length)
+                row = 0;
+            count = 0;
+            k = row;
+            for (int col = a[0].length - 1; col >= 0 && k < a.length; col--) {
+                if (rowCount > a.length) {
+                    col = (a.length - 1) - (rowCount % a.length);
+                }
+                while (k < a.length - 1 && col > 0) {
+                    if (a[k][col] == a[k + 1][col - 1] - 1) {
+                        count++;
+                        if (count >= 3) {
+                            return true;
+                        }                    
+                    }
+                    k++;
+                    col--;
+                }
+            }
+        }
+        
+        rowCount = 0;
+        count = 0;
+        //loop to check if consecutively decreasing
+        for (int row = 0; rowCount <= a.length * 2; row++) {    
+            rowCount++;
+            if (rowCount > a.length)
+                row = 0;
+            count = 0;
+            k = row;
+            for (int col = a[0].length - 1; col >= 0 && k < a.length; col--) {
+                if (rowCount > a.length) {
+                    col = (a.length - 1) - (rowCount % a.length);
+                }
+                while (k < a.length - 1 && col > 0) {
+                    if (a[k][col] == a[k + 1][col - 1] + 1) {
+                        count++;
+                        if (count >= 3) {
+                            return true;
+                        }                    
+                    }
+                    k++;
+                    col--;
+                }
+            }
+        }
+        return false;
+    }
 }
+    
